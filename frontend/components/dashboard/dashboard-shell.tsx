ad9986b5f4c93533c654d257b4012a2907ae62a7
devrahulbanjara <rdbanjara07@gmail.com>
2025-03-16 09:05:39 +0545

    Remove unnecessary files

--- frontend/components/dashboard/dashboard-shell.tsx
+++
@@ -1,9 +0,0 @@
-import type React from "react"
-interface DashboardShellProps {
-  children: React.ReactNode
-}
-
-export function DashboardShell({ children }: DashboardShellProps) {
-  return <div className="flex flex-col gap-4 pb-10">{children}</div>
-}
-
