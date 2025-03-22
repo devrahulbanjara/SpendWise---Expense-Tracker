337eb0c55ddcc5e95dcac0b8a5132c7c1f304879
devrahulbanjara <rdbanjara07@gmail.com>
2025-03-22 07:52:30 +0545

    Dashboard User summary API
    - fetches total income, total expenses and remaining balance
    - api to add income and expense
    - api to create new profile and switch between them

--- backend/core/config.py
+++
@@ -1,9 +0,0 @@
-from fastapi import Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer
-from core.security import decode_jwt_token
-
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
-
-async def get_current_user(token: str = Depends(oauth2_scheme)):
-    """Extracts and validates user from JWT token."""
-    return decode_jwt_token(token)
